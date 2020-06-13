695d730aba0b5c74d1d1d137d6e8fa67e9ae42f6
Vitaliy <animus2393@gmail.com>
2020-06-13 20:23:16 +0300

    create architect for project

---
+++ resources/views/index.blade.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <title>nomer</title>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <meta property="og:url" content="https://top100awards.ru">
+    <meta property="og:title" content="NOMER">
+    <meta property="og:description" content="">
+    <meta property="og:type" content="website">
+    <meta name="og:image" itemprop="image" content="{{env("APP_URL")}}/img/ogImage.jpg">
+    <meta property="fb:app_id" content="257953674358265">
+    <meta name="format-detection" content="telephone=no">
+    <link rel="canonical" href="/">
+    <link rel="shortcut icon" href="/img/favicon.ico" type="image/x-icon">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900&display=swap&subset=cyrillic" rel="stylesheet">
+{{--    <link rel="stylesheet" href="/fonts.css" media="none" onload="this.media='all'">--}}
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+</head>
+<body>
+
+@include('sprite')
+@include('components.header.header')
+<main>
+    @yield('nomer-content')
+</main>
+
+@include('components.footer.footer')
+
+<script src="{{ mix('js/app.js') }}"></script>
+
+</body>
+</html>
---
+++ resources/views/pages/nomer-content/nomer-contect.blade.php
@@ -0,0 +1,9 @@
+@extends('index')
+
+
+@section('nomer-content')
+    @include('components.section-1.section-1')
+    @include('components.section-2.section-2')
+    @include('components.section-3.section-3')
+    @include('components.section-4.section-4')
+@endsection
