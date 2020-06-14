7b8b7afbced6f73a132c4106b00fb3378f798910
Vitaliy <animus2393@gmail.com>
2020-06-15 00:30:58 +0300

    add management-buttons in section-1

--- resources/views/components/section-1/section-1.blade.php
+++ resources/views/components/section-1/section-1.blade.php
@@ -3,19 +3,48 @@
 
     <div class="background-wrapper">
         <div class="background-image">
-            <div class="management container">
-                <div class="management-buttons">
-                    <div class="management-btn mod-active">
-                        <span>Стоимость</span>
+            <div class="main-wrapper">
+                <div class="management container">
+                    <div class="management-buttons">
+                        <div class="management-btn mod-active">
+                            <span>Стоимость</span>
+                        </div>
+                        <div class="management-btn">
+                            <span>Законность</span>
+                        </div>
+                        <div class="management-btn">
+                            <span>Доставка и оплата</span>
+                        </div>
+                        <div class="management-btn">
+                            <span>Отзывы</span>
+                        </div>
                     </div>
-                    <div class="management-btn">
-                        <span>Законность</span>
-                    </div>
-                    <div class="management-btn">
-                        <span>Доставка и оплата</span>
-                    </div>
-                    <div class="management-btn">
-                        <span>Отзывы</span>
+
+                    <div class="management-content">
+                        <div class="management-description">
+                            <h1 class="management-description-title">
+                                <span>Lorem ipsum</span>
+                                dolor sit amet, consectetur adipiscing</h1>
+
+                            <p class="management-description-subtitle">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do&nbsp;eiusmod tempor
+                                incididunt ut&nbsp;labore et&nbsp;dolore magna aliqua. Quis ipsum suspendisse ultrices
+                                gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.</p>
+                        </div>
+
+                        <div class="management-forms">
+                            <div class="management-form-item">
+                                <span class="form-item_title">Ваше имя</span>
+                                <input class="form-item_input" type="text" placeholder="Ваше имя">
+                            </div>
+                            <div class="management-form-item">
+                                <span class="form-item_title">Телефон</span>
+                                <input class="form-item_input" type="text" placeholder="Телефон">
+                            </div>
+                            <div class="management-form-item">
+                                <span  class="form-item_title">Ваши номера</span>
+                                <input class="form-item_input" type="text" placeholder="Ваши номера">
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
