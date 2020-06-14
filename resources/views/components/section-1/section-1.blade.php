47493f12460990afb3baceefaa7fdad3d121e8df
Vitaliy <animus2393@gmail.com>
2020-06-14 22:31:01 +0300

    add management-buttons in section-1

--- resources/views/components/section-1/section-1.blade.php
+++ resources/views/components/section-1/section-1.blade.php
@@ -4,7 +4,20 @@
     <div class="background-wrapper">
         <div class="background-image">
             <div class="management container">
-
+                <div class="management-buttons">
+                    <div class="management-btn mod-active">
+                        <span>Стоимость</span>
+                    </div>
+                    <div class="management-btn">
+                        <span>Законность</span>
+                    </div>
+                    <div class="management-btn">
+                        <span>Доставка и оплата</span>
+                    </div>
+                    <div class="management-btn">
+                        <span>Отзывы</span>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
