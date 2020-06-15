dd56401bc14a0f8c5712afc11c2bbe13b9b96351
Vitaliy <animus2393@gmail.com>
2020-06-15 15:25:48 +0300

    fix swiper

--- resources/views/components/section-4/section-4.js
+++ resources/views/components/section-4/section-4.js
@@ -16,4 +16,25 @@ function initSwiper() {
     });
 }
 
-initSwiper();
+
+//При загрузке страницы на разрешении только  < 1200 swiper загружается
+$(window).on('load', function () {
+    if ($(window).width() < 1200) {
+        initSwiper();
+    }
+});
+//При ресайзе страницы на разрешении только < 1200 swiper обновляется
+$(window).on('resize', function () {
+    if ($(window).width() < 1200) {
+        if (commentMobileSwiper) {
+            commentMobileSwiper.update();
+        } else {
+            initSwiper();
+        }
+    } else {
+        if (commentMobileSwiper) {
+            commentMobileSwiper.destroy();
+            commentMobileSwiper = false;
+        }
+    }
+});
